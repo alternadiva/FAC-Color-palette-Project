40801a0dbbe0a4b5eb118c25f17302d9b8469984
alternadiva <85565743+alternadiva@users.noreply.github.com>
2022-01-09 22:25:03 +0000

    html, css, js files created

---
+++ script.js
@@ -0,0 +1,6 @@
+let color1 = document.getElementById("color1");
+let color2 = document.getElementById("color2");
+let color3 = document.getElementById("color3");
+let color4 = document.getElementById("color4");
+
+window.addEventListener('load', randomStartColor);
