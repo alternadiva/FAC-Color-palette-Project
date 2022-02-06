8f4989d9a1418de5a4346db02395e7bd6ea962e8
alternadiva <asomeonelse@gmail.com>
2022-02-06 19:08:21 +0100

    refactor js code

--- script.js
+++ script.js
@@ -1,39 +1,37 @@
-let color1 = document.getElementById("color1");
-let color2 = document.getElementById("color2");
-let color3 = document.getElementById("color3");
-let color4 = document.getElementById("color4");
+let palette = document.getElementById("palette");
 
-let  button1 = document.getElementById("button1");
-let  button2 = document.getElementById("button2");
-let  button3 = document.getElementById("button3");
-let  button4 = document.getElementById("button4");
-
-function randomStartColor1() {
-    let randomColor1 = Math.floor(Math.random() * 16777215).toString(16); //Hexadecimal
-    color1.style.backgroundColor = "#" + randomColor1;
+function colorFields() {
+    for (let i = 0; i < 4; i++) {
+        let color = document.createElement("div");
+        color.className = "colors";
+        color.setAttribute("id", `color${[i+1]}`);
+        let buttonChange = document.createElement("button");
+        buttonChange.innerText = "Change";
+        let buttonKeep = document.createElement("button");
+        buttonKeep.innerText = "Keep";
+        color.appendChild(buttonChange);
+        color.appendChild(buttonKeep);
+        palette.appendChild(color);
+    }
 }
 
-function randomStartColor2() {
-    let randomColor2 = Math.floor(Math.random() * 16777215).toString(16);
-    color2.style.backgroundColor = "#" + randomColor2;
+let colors = document.getElementsByClassName("colors");
+
+function addRandomColor() {
+    for (let i = 0; i < colors.length; i++) {
+        let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        colors[i].style.background = "#" + randomColor;
+    }
 }
 
-function randomStartColor3() {
-    let randomColor3 = Math.floor(Math.random() * 16777215).toString(16);
-    color3.style.backgroundColor = "#" + randomColor3;
+function allWindowLoadFuncs() {
+    colorFields();
+    addRandomColor();
 }
 
-function randomStartColor4() {
-    let randomColor4 = Math.floor(Math.random() * 16777215).toString(16);
-    color4.style.backgroundColor = "#" + randomColor4;
-}
+window.addEventListener('load', allWindowLoadFuncs);
 
-window.addEventListener('load', randomStartColor1);
-window.addEventListener('load', randomStartColor2);
-window.addEventListener('load', randomStartColor3);
-window.addEventListener('load', randomStartColor4);
+/* let allButtons = document.getElementsByTagName("button");
+let buts = Array.from(allButtons);
 
-button1.addEventListener('click', randomStartColor1);
-button2.addEventListener('click', randomStartColor2);
-button3.addEventListener('click', randomStartColor3);
-button4.addEventListener('click', randomStartColor4);
+buts.forEach(x => x.addEventListener("click", addRandomColor)) */
