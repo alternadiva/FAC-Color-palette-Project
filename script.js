196443d734b580b808efa1c90a102e49c412895c
alternadiva <asomeonelse@gmail.com>
2022-02-07 13:48:16 +0100

    set colors to local storage

--- script.js
+++ script.js
@@ -4,17 +4,20 @@ window.addEventListener('load', allWindowLoadFuncs);
 
 function colorFields() {
     for (let i = 0; i < 4; i++) {
+        // create color divs and add to DOM
         let color = document.createElement("div");
         color.className = "colors";
         color.setAttribute("id", `color${[i+1]}`);
         palette.appendChild(color);
+
+        // create buttons and add to DOM
         let buttonChange = document.createElement("button");
         buttonChange.innerText = "Change";
         buttonChange.className = "change-btn";
         let buttonKeep = document.createElement("button");
         buttonKeep.innerText = "Keep";
-        color.appendChild(buttonChange);
-        color.appendChild(buttonKeep);
+        buttonKeep.className = "keep-btn";
+        color.append(buttonChange, buttonKeep);
     }
 }
 
@@ -31,13 +34,22 @@ let changeButtons = document.getElementsByClassName("change-btn");
 
 function buttonClrChange() {
     let randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    this.parentElement.style.background = "#" + randomColor;
+    this.parentElement.style.background = JSON.parse(localStorage.getItem("colors")) || "#" + randomColor;
+}
+
+let keepButtons = document.getElementsByClassName("keep-btn");
+let storage = [];
+
+function keepInStorage() {
+    let keptColor = this.parentElement.style.background;
+    localStorage.setItem("colors", keptColor);
 }
 
 function allWindowLoadFuncs() {
     colorFields();
     addRandomColor();
     Array.from(changeButtons).forEach(button => button.addEventListener("click", buttonClrChange));
+    Array.from(keepButtons).forEach(button => button.addEventListener("click", keepInStorage));
 }
 
 
